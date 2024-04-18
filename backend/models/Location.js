7cce898e2beeb04d89913af3b56244de8c92712e
kumailDoc <113384537+kumailDoc@users.noreply.github.com>
2024-04-18 13:39:00 -0400

    Delete backend directory

--- backend/models/Location.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LocationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  pincode: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Location', LocationSchema);
--- backend/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateUser } = require('../middleware/authMiddleware');
-
-router.get('/',authenticateUser, (req, res) => {
-  res.send('testing Welcome to the Dashboard');
-});
-
-module.exports = router;
